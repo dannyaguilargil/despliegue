91a91547a1557eea3af19dccbe1ebec92ac42389
dannyaguilargilGH <52986710+dannyaguilargil@users.noreply.github.com>
2022-11-19 23:20:37 -0500

    condicional de error

--- Servidor/conexion.php
+++ Servidor/conexion.php
@@ -6,5 +6,11 @@ $DB_PASSWORD=$_ENV["DB_PASSWORD"];
 $DB_NAME=$_ENV["DB_NAME"];
 $DB_PORT=$_ENV["DB_PORT"];
 $db=mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
+if(!$db) {
+        echo "ERROR AL CONECTAR A LA BASE DE DATOS";
+}
+else{
+       // echo "CONECTADO A LA BASE DE DATOS";
+}
 ?>
 
