f2a3a52f5fef0ac1fd6f33e2a3fc8babcd5affa0
dannyaguilargilGH <52986710+dannyaguilargil@users.noreply.github.com>
2022-11-21 17:08:32 -0500

    Correcion de la conexion por variables de entorno

--- Servidor/conexion2.php
+++ Servidor/conexion2.php
@@ -1,14 +1,17 @@
 <?php
 
-$localhost = $_ENV['localhost'];
-$usuario = $_ENV['danny'];
-$password = $_ENV['danny'];
-$bd =  $_ENV['sistema_nuevo'];
+<?php 
 
-/* Creando una nueva conexión a la base de datos. */
-$conn = new mysqli("$localhost", "$usuario", "$password", "$bd");
-
-/* Comprobando si hay un error de conexión. */
-if ($conn->connect_error) {
-    die('Error de conexion ' . $conn->connect_error);
+$DB_HOST=$_ENV["DB_HOST"];
+$DB_USER=$_ENV["DB_USER"];
+$DB_PASSWORD=$_ENV["DB_PASSWORD"];
+$DB_NAME=$_ENV["DB_NAME"];
+$DB_PORT=$_ENV["DB_PORT"];
+$conn=mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
+if(!$db) {
+        echo "ERROR AL CONECTAR A LA BASE DE DATOS";
 }
+else{
+       // echo "CONECTADO A LA BASE DE DATOS";
+}
+?>
